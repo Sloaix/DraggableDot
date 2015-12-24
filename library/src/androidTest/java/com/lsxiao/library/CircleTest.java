491cd1347ccdc35c3caccc9d3bd8bcac54029c03
lsxiao <faith.epiphone@qq.com>
2015-12-25 02:57:17 +0800

    cut point calculate

--- library/src/androidTest/java/com/lsxiao/library/CircleTest.java
+++ library/src/androidTest/java/com/lsxiao/library/CircleTest.java
@@ -2,6 +2,7 @@ package com.lsxiao.library;
 
 import android.graphics.PointF;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
 /**
  * author lsxiao
@@ -35,5 +36,38 @@ public class CircleTest extends AndroidTestCase {
     public void testRightAngleSizeLength() throws Exception {
         //勾三股四弦五
         assertEquals(DotView.getLegLength(5, 3), 4f);
+    }
+
+    public void testQiedian() throws Exception {
+        Log.d("xls", cal(new PointF(0, 0), new PointF(18.72f, -1.72f), 10,true) + "");
+    }
+
+    PointF cal(PointF pCenter, PointF pOutside, float radius, boolean firstPoint) {
+        PointF E = new PointF(0, 0);
+        PointF F = new PointF(0, 0);
+        PointF G = new PointF(0, 0);
+        PointF H = new PointF(0, 0);
+
+        //1. 坐标平移到圆心ptCenter处,求园外点的新坐标E
+        E.x = pOutside.x - pCenter.x;
+        E.y = pOutside.y - pCenter.y; //平移变换到E
+
+        //2. 求园与OE的交点坐标F, 相当于E的缩放变换
+        float t = radius / (float) Math.sqrt(E.x * E.x + E.y * E.y);  //得到缩放比例
+        F.x = E.x * t;
+        F.y = E.y * t;   //缩放变换到F
+
+        //3. 将E旋转变换角度a到切点G，其中cos(a)=r/OF=t, 所以a=arccos(t);
+        float a = firstPoint ? (float) Math.acos(t) : (float) -Math.acos(t);  //得到旋转角度
+        G.x = F.x * (float) Math.cos(a) - F.y * (float) Math.sin(a);
+        G.y = F.x * (float) Math.sin(a) + F.y * (float) Math.cos(a);    //旋转变换到G
+
+        //4. 将G平移到原来的坐标下得到新坐标H
+        H.x = G.x + pCenter.x;
+        H.y = G.y + pCenter.y;             //平移变换到H
+
+        //5. 返回H
+        return new PointF(H.x, H.y);
+        //6. 实际应用过程中，只要一个中间变量E,其他F,G,H可以不用。
     }
 }
