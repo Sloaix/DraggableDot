5f27d296dd8badbfb29266809519d175c52447af
lsxiao <faith.epiphone@qq.com>
2015-12-26 03:14:05 +0800

    finish drag event handler.

--- library/src/main/java/com/lsxiao/library/DraggableLayout.java
+++ library/src/main/java/com/lsxiao/library/DraggableLayout.java
@@ -1,13 +1,14 @@
 package com.lsxiao.library;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
-import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +19,7 @@ import android.widget.FrameLayout;
  * date:2015/12/25 17:02
  */
 public class DraggableLayout extends FrameLayout {
-    ViewDragHelper mViewDragHelper;
     DotView mDotView;
-    private int mLeft;
-    private int mTop;
     Circle mDragCircle;
     Circle mFixedCircle;
     PointF mFixedPointA;
@@ -32,8 +30,21 @@ public class DraggableLayout extends FrameLayout {
     PointF mOperationPointB;
     //画笔
     Paint mPaint;
-
+    //路径
     Path mPath;
+
+    float mStartPosX;
+    float mStartPosY;
+
+    float mLastPosX;
+    float mLastPosY;
+
+    /**
+     * 是否可以拖动
+     */
+    public static boolean mDraggable = false;
+
+    public static ValueAnimator valueAnimator;
 
     public DraggableLayout(Context context) {
         this(context, null);
@@ -60,29 +71,15 @@ public class DraggableLayout extends FrameLayout {
             throw new NullPointerException("you must init first");
         }
 
-        //DotView的父布局
-        ViewGroup dotViewParent = (ViewGroup) mDotView.getParent();
-
         //实例化拖动助手
-        mViewDragHelper = ViewDragHelper.create(dotViewParent, 1.0f, new DragCallback());
         mPaint = new Paint();
+        mPaint.setAntiAlias(true);
         mPaint.setStrokeWidth(2);
         mPaint.setColor(Color.BLACK);
         mPaint.setStyle(Paint.Style.FILL);
 
-//        if (mFixedCircle == null) {
-//            mFixedCircle = new Circle((mDotView.getLeft() + mDotView.getMeasuredWidth()) / 2, (mDotView.getTop() + mDotView.getMeasuredHeight()) / 2, mDotView.getMeasuredWidth() / 2);
-//        }
-//
-//        if (mDragCircle == null) {
-//            mDragCircle = mFixedCircle;
-//        }
-        mFixedCircle = new Circle(500, 500, 20);
-        mDragCircle = new Circle(300, 500, 30);
-        update();
         mPath = new Path();
     }
-
 
     private DotView findDotView(ViewGroup viewGroup) {
         for (int i = 0; i < viewGroup.getChildCount(); i++) {
@@ -102,23 +99,34 @@ public class DraggableLayout extends FrameLayout {
      * @param event MotionEvent
      * @return true if touched on dot view
      */
-    private boolean isTouchOnDot(MotionEvent event) {
-        if (mDotView.getVisibility() != VISIBLE) {
-            return false;
-        }
+    private boolean isTouchDot(MotionEvent event) {
         int[] location = new int[2];
         mDotView.getLocationOnScreen(location);
-        int upperLimit = location[1] + mDotView.getMeasuredHeight();
-        int lowerLimit = location[1];
-        int y = (int) event.getRawY();
-        return (y > lowerLimit && y < upperLimit);
+        final int w = mDotView.getMeasuredWidth();
+        final int h = mDotView.getMeasuredHeight();
+        int posX = (int) event.getRawX();
+        int posY = (int) event.getRawY();
+
+        int lowerX = location[0];
+        int upperX = w + location[0];
+
+        int lowerY = location[1];
+        int upperY = h + location[1];
+        log(lowerX, upperX);
+        log(lowerY, upperY);
+        log(posX, posY);
+        return (posX > lowerX && posX < upperX && posY > lowerY && posY < upperY);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        update();
-        mFixedCircle.draw(canvas, mPaint);
-        mDragCircle.draw(canvas, mPaint);
+        if (mFixedCircle == null || mDragCircle == null) {
+            initCircle();
+        }
+
+        calculatePoint();
+        canvas.drawCircle(mFixedCircle.mCenter.x, mFixedCircle.mCenter.y, mFixedCircle.mRadius, mPaint);
+        canvas.drawCircle(mDragCircle.mCenter.x, mDragCircle.mCenter.y, mDragCircle.mRadius, mPaint);
         drawDrag(canvas);
     }
 
@@ -133,28 +141,10 @@ public class DraggableLayout extends FrameLayout {
         canvas.drawPath(mPath, mPaint);
     }
 
-//    public void update() {
-//        mFixedPointA = mFixedCircle.getCutPoint(mDragCircle.mCenter, true);
-//        mDragPointA = mDragCircle.getCutPoint(mFixedCircle.mCenter, false);
-//
-//        float oxa = (mFixedPointA.x + mDragPointA.x) / 2;
-//        float oya = (mFixedPointA.y + mDragPointA.y) / 2;
-//        mOperationPointA = new PointF(oxa, oya);
-//
-//        mFixedPointB = mFixedCircle.getCutPoint(mDragCircle.mCenter, false);
-//        mDragPointB = mDragCircle.getCutPoint(mFixedCircle.mCenter, true);
-//
-//
-//        float oxb = (mFixedPointB.x + mDragPointB.x) / 2;
-//        float oyb = (mFixedPointB.y + mDragPointB.y) / 2;
-//        mOperationPointB = new PointF(oxb, oyb);
-//    }
-
-    public void update() {
-//        float length = mDragCircle.lengthBetweenCenter(mFixedCircle);
-//        float fac = 0.1f * length;
-//        mFixedCircle.mRadius += mDragCircle.mRadius * fac;
-
+    /**
+     * 计算贝塞尔曲线的六个点
+     */
+    public void calculatePoint() {
         mFixedPointA = mFixedCircle.getCutPoint(mDragCircle.mCenter, true);
         mDragPointA = mDragCircle.getCutPoint(mFixedCircle.mCenter, false);
 
@@ -169,40 +159,94 @@ public class DraggableLayout extends FrameLayout {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent event) {
-        boolean isIntercept = mViewDragHelper.shouldInterceptTouchEvent(event);
-        return isTouchOnDot(event);
+        if (isTouchDot(event) && mDotView.getVisibility() == VISIBLE) {
+            mDraggable = true;
+            return true;
+        } else {
+            return super.onInterceptTouchEvent(event);
+        }
+    }
+
+    /**
+     * 保存最后一次Motion
+     *
+     * @param ev MotionEvent
+     */
+    private void saveLastMotion(MotionEvent ev) {
+        mLastPosX = ev.getX();
+        mLastPosY = ev.getY();
+    }
+
+    /**
+     * 保存开始的Motion
+     *
+     * @param ev MotionEvent
+     */
+    private void saveStartMotion(MotionEvent ev) {
+        mStartPosX = ev.getX();
+        mStartPosY = ev.getY();
+    }
+
+    private void log(MotionEvent ev) {
+        Log.d("xls", ev.getX() + "");
+        Log.d("xls", ev.getY() + "");
+    }
+
+    private void log(int x, int y) {
+        Log.d("xls", x + "  " + y);
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        mViewDragHelper.processTouchEvent(event);
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (!mDraggable) {
+            return false;
+        }
+        final int action = ev.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN: {
+                saveStartMotion(ev);
+                saveLastMotion(ev);
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                final float x = ev.getX();
+                final float y = ev.getY();
+                final int dx = (int) (x - mLastPosX);
+                final int dy = (int) (y - mLastPosY);
+                mDragCircle.mCenter.x += dx;
+                mDragCircle.mCenter.y += dy;
+                saveLastMotion(ev);
+                break;
+            }
+
+            case MotionEvent.ACTION_UP: {
+                mDraggable = false;
+                resetCircle();
+            }
+
+            case MotionEvent.ACTION_CANCEL: {
+                resetCircle();
+                break;
+            }
+        }
+        invalidate();
         return true;
     }
 
-    class DragCallback extends ViewDragHelper.Callback {
-        @Override
-        public boolean tryCaptureView(View child, int pointerId) {
-            return child instanceof DotView;
-        }
-
-        @Override
-        public int clampViewPositionVertical(View child, int top, int dy) {
-            if (child.getTop() != top) {
-                mTop = top;
-                mDragCircle.mCenter.y += dy;
-                invalidate();
-            }
-            return child.getTop();
-        }
-
-        @Override
-        public int clampViewPositionHorizontal(View child, int left, int dx) {
-            if (child.getLeft() != left) {
-                mLeft = left;
-                mDragCircle.mCenter.x += dx;
-                invalidate();
-            }
-            return child.getLeft();
-        }
+    private void resetCircle() {
+        initCircle();
     }
+
+    private void initCircle() {
+        final int[] dotLocation = new int[2];
+        final int[] layoutLocation = new int[2];
+        mDotView.getLocationInWindow(dotLocation);
+        getLocationInWindow(layoutLocation);
+        int dx = -layoutLocation[0] + dotLocation[0];
+        int dy = -layoutLocation[1] + dotLocation[1];
+        log(layoutLocation[0] - dotLocation[0], layoutLocation[1] - dotLocation[1]);
+        mFixedCircle = new Circle(dx - getLeft() + mDotView.getWidth() / 2, dy - getTop() + mDotView.getWidth() / 2, mDotView.getWidth() / 2);
+        mDragCircle = new Circle(mFixedCircle.mCenter.x, mFixedCircle.mCenter.y, mFixedCircle.mRadius);
+    }
+
 }
