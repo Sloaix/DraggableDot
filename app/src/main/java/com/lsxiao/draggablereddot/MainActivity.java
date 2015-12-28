86b0f7bd52ad6d3bed1249c30e75314372b78379
LsXiao <faith.epiphone@qq.com>
2015-12-28 21:58:38 +0800

    add other states to  make code logic clear

--- app/src/main/java/com/lsxiao/draggablereddot/MainActivity.java
+++ app/src/main/java/com/lsxiao/draggablereddot/MainActivity.java
@@ -17,8 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        DraggableLayout.register(this);
+        DraggableLayout.bind(this);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(new SimpleAdapter());
