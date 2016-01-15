7cd6b0dfbb60a3fd68a90311407c4411b1547c10
LsXiao <faith.epiphone@qq.com>
2016-01-15 17:15:02 +0800

    update simple

--- app/src/main/java/com/lsxiao/draggablereddot/activity/MainActivity.java
+++ app/src/main/java/com/lsxiao/draggablereddot/activity/MainActivity.java
@@ -2,38 +2,29 @@ package com.lsxiao.draggablereddot.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.SimpleAdapter;
 
 import com.lsxiao.draggablereddot.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private RecyclerView mRecyclerView;
-    private SimpleAdapter mSimpleAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View vAppLayout1 = findViewById(R.id.tv_tab_layout_1);
-        View vAppLayout2 = findViewById(R.id.tv_tab_layout_2);
+        View vAppLayout1 = findViewById(R.id.tv_tab_layout);
         View vListLayout = findViewById(R.id.tv_list_layout);
 
         vAppLayout1.setOnClickListener(this);
-        vAppLayout2.setOnClickListener(this);
         vListLayout.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.tv_tab_layout_1: {
+            case R.id.tv_tab_layout: {
                 TabActivity.start(this);
-                break;
-            }
-            case R.id.tv_tab_layout_2: {
                 break;
             }
             case R.id.tv_list_layout: {
